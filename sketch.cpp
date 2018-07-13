41edc05ffaf396502102e7580414cfab35300bc6
Hans Lehnert <hans.lehnert@gmail.com>
2018-07-13 17:17:38 -0400

    Move parsing into separate function

--- sketch.cpp
+++ sketch.cpp
@@ -11,37 +11,35 @@ const unsigned int N_HASH = 4;
 const unsigned int M = 14;
 
 
-unsigned int hash(unsigned int key, unsigned int* seeds) {
+unsigned int hashH3(unsigned int key, unsigned int* seeds) {
     unsigned int result = 0;
-    while (key) {
+    for (int i = 0; i < 32; i++) {
         if (key & 1)
-            result ^= seeds[0];
+            result ^= seeds[i];
         key >>= 1;
-        seeds++;
     }
     return result;
 }
 
 
-int main(int argc, char* argv[]) {
-    // Parse data set
-    std::ifstream dataset_file("control.fasta");
-    std::vector<int> data_vectors;
+std::vector<unsigned int> parseFasta(std::istream& input) {
+    std::vector<unsigned int> data_vectors;
 
-    while (!dataset_file.eof()) {
+    while (!input.eof()) {
         std::string line;
-        dataset_file >> line;
+        input >> line;
 
+        // Skip line
         if (line[0] == '>') {
             continue;
         }
 
-        int sequence = 0;
+        unsigned int sequence = 0;
         for (unsigned int i = 0; i < line.length(); i++) {
-            sequence << 2;
+            sequence <<= 2;
             switch(line[i]) {
             case 'A':
-                sequence |= 0;;
+                sequence |= 0;
                 break;
             case 'C':
                 sequence |= 1;
@@ -58,6 +56,16 @@ int main(int argc, char* argv[]) {
                 data_vectors.push_back(sequence);
         }
     }
+
+    return data_vectors;
+}
+
+
+int main(int argc, char* argv[]) {
+    // Parse data set
+    std::ifstream dataset_file("control.fasta");
+    std::vector<unsigned int> data_vectors = parseFasta(dataset_file);
+    dataset_file.close();
 
     std::cout << "Data vectors: " << data_vectors.size() << std::endl;
 
@@ -79,22 +87,25 @@ int main(int argc, char* argv[]) {
         unsigned int min_func = 0;
         unsigned int min_hash = 0;
         for (unsigned int j = 0; j < N_HASH; j++) {
-            unsigned int hash_result = hash(data_vectors[i], seeds[j]);
+            unsigned int hash_result = hashH3(data_vectors[i], seeds[j]);
             if (sketch[j][hash_result] < min_hits) {
                 min_hits = sketch[j][hash_result];
                 min_func = j;
                 min_hash = hash_result;
             }
-            sketch[min_func][min_hash]++;
         }
+        sketch[min_func][min_hash]++;
     }
 
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> diff = end - start;
     std::cout << "Execution time: " << diff.count() << " s" << std::endl;
 
-    std::ofstream output_file("sketch.txt", std::ios::binary);
+    // Write sketch to file, just to prevent the compiler to optimize out the
+    // process. There is probably a better way...
+    std::ofstream output_file("sketch.bin", std::ios::binary);
     output_file.write((char*)&sketch[0][0], N_HASH * (1 << M) * sizeof(int));
+    output_file.close();
 
     return 0;
 }
