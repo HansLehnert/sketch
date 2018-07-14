b1a10536048bde50cc08be87030eddeaaba56354
Hans Lehnert <hans.lehnert@gmail.com>
2018-07-13 20:35:08 -0400

    Fix implementation error. Use unsigned short for seeds for better performance.

--- sketch.cpp
+++ sketch.cpp
@@ -2,16 +2,20 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <cstdlib>
 #include <chrono>
 #include <limits>
+#include <unordered_set>
 
 
 const unsigned int N_HASH = 4;
 const unsigned int M = 14;
+const unsigned int RHO = 145;
 
 
-unsigned int hashH3(unsigned int key, unsigned int* seeds) {
+/**
+ * @brief Compute H3 hash
+ */
+unsigned int hashH3(unsigned int key, unsigned short* seeds) {
     unsigned int result = 0;
     for (int i = 0; i < 32; i++) {
         if (key & 1)
@@ -22,7 +26,14 @@ unsigned int hashH3(unsigned int key, unsigned int* seeds) {
 }
 
 
-std::vector<unsigned int> parseFasta(std::istream& input) {
+/**
+ * @brief Extract k-mers from DNA sequences in a .fasta file
+ * 
+ * @param input Input stream from where the .fasta file will be read
+ * @param length Length of the k-mers
+ * @return std::vector<unsigned int> 
+ */
+std::vector<unsigned int> parseFasta(std::istream& input, int length) {
     std::vector<unsigned int> data_vectors;
 
     while (!input.eof()) {
@@ -52,7 +63,7 @@ std::vector<unsigned int> parseFasta(std::istream& input) {
                 break;
             }
 
-            if (i >= 15)
+            if (i >= length - 1)
                 data_vectors.push_back(sequence);
         }
     }
@@ -62,50 +73,81 @@ std::vector<unsigned int> parseFasta(std::istream& input) {
 
 
 int main(int argc, char* argv[]) {
-    // Parse data set
-    std::ifstream dataset_file("control.fasta");
-    std::vector<unsigned int> data_vectors = parseFasta(dataset_file);
-    dataset_file.close();
-
-    std::cout << "Data vectors: " << data_vectors.size() << std::endl;
-
     // Generate hash vectors
-    unsigned int seeds[N_HASH][32];
+    unsigned short seeds[N_HASH][32];
     for (unsigned int i = 0; i < N_HASH; i++) {
         for (unsigned int j = 0; j < 32; j++) {
             seeds[i][j] = rand() & ((1 << M) - 1);
         }
     }
 
-    // Create sketch and hash
+    // Create sketch
     unsigned int sketch[N_HASH][1 << M] = {0};
-
+    
+    // Start time measurement
     auto start = std::chrono::steady_clock::now();
+    
+    // Parse data set
+    std::ifstream dataset_file("data/test.fasta");
+    std::vector<unsigned int> data_vectors = parseFasta(dataset_file, 16);
+    dataset_file.close();
 
+    std::unordered_set<unsigned int> heavy_hitters;
+    heavy_hitters.reserve(data_vectors.size() / 10);
+
+    // Hash values
     for (unsigned int i = 0; i < data_vectors.size(); i++) {
         unsigned int min_hits = std::numeric_limits<unsigned int>::max();
-        unsigned int min_func = 0;
-        unsigned int min_hash = 0;
+        unsigned int hashes[N_HASH];
+
         for (unsigned int j = 0; j < N_HASH; j++) {
-            unsigned int hash_result = hashH3(data_vectors[i], seeds[j]);
-            if (sketch[j][hash_result] < min_hits) {
-                min_hits = sketch[j][hash_result];
-                min_func = j;
-                min_hash = hash_result;
+            hashes[j] = hashH3(data_vectors[i], seeds[j]);
+            if (sketch[j][hashes[j]] < min_hits) {
+                min_hits = sketch[j][hashes[j]];
             }
         }
-        sketch[min_func][min_hash]++;
+
+        for (unsigned int j = 0; j < N_HASH; j++) {
+            if (sketch[j][hashes[j]] == min_hits) {
+                sketch[j][hashes[j]]++;
+            }
+        }
+
+        if (min_hits + 1 >= RHO) {
+            heavy_hitters.insert(data_vectors[i]);
+        }
     }
 
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> diff = end - start;
+
+    std::cout << "Data vectors: " << data_vectors.size() << std::endl;
     std::cout << "Execution time: " << diff.count() << " s" << std::endl;
 
-    // Write sketch to file, just to prevent the compiler to optimize out the
-    // process. There is probably a better way...
-    std::ofstream output_file("sketch.bin", std::ios::binary);
-    output_file.write((char*)&sketch[0][0], N_HASH * (1 << M) * sizeof(int));
-    output_file.close();
+    // Write heavy-hitters to output file
+    std::ofstream heavy_hitters_file("heavy-hitters.txt");
+    for (auto x : heavy_hitters) {
+        std::string sequence;
+
+        for (int i = 0; i < 16; i++) {
+            switch (x << (i * 2) >> 30) {
+            case 0:
+                sequence += 'A';
+                break;
+            case 1:
+                sequence += 'C';
+                break;
+            case 2:
+                sequence += 'T';
+                break;
+            case 3:
+                sequence += 'G';
+                break;
+            }
+        }
+        heavy_hitters_file << sequence << std::endl;
+    }
+    heavy_hitters_file.close();
 
     return 0;
 }
