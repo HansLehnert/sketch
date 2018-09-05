6b2cd4f7c7d53f92663c434f13e4aac722431d6f
Hans Lehnert <hans.lehnert@gmail.com>
2018-09-04 21:25:05 -0300

    Measure times using nvprof for cuda programs and select programs to run through CLI

--- run_eval.py
+++ run_eval.py
@@ -4,6 +4,14 @@ import subprocess
 import collections
 import re
 import csv
+import argparse
+import sys
+
+# Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('runs', type=int)
+parser.add_argument('type', nargs='*')
+args = parser.parse_args(sys.argv[1:])
 
 # Detect CUDA and AVX features
 try:
@@ -20,15 +28,15 @@ else:
     use_avx = False
 
 # Programs to check
-programs = []
+programs = {}
 
-# programs.append('bin/sketch')
+programs['bin/sketch'] = {'default'}
 
-# if use_avx:
-#     programs.append('bin/sketch_avx_pipelined')
+if use_avx:
+    programs['bin/sketch_avx_pipelined'] = {'avx'}
 
 if use_cuda:
-    programs.append('bin/sketch_cu')
+    programs['bin/sketch_cu'] = {'cuda'}
 
 # Database description
 datasets = {
@@ -72,10 +80,14 @@ datasets = {
     }
 }
 
-
-n_runs = 3
+n_runs = args.runs
 
 for program_name in programs:
+    if (args.type is not None and
+            not any(x in programs[program_name] for x in args.type)):
+        print('Skipping program {}'.format(program_name))
+        continue
+
     print('Running program {}'.format(program_name))
 
     runs = collections.OrderedDict()
@@ -89,43 +101,64 @@ for program_name in programs:
             min_length = dataset['first_length']
             max_length = min_length + len(dataset['thresholds']) - 1
 
+            command = [program_name,
+                dataset['test_file'],
+                dataset['control_file'],
+                str(min_length),
+                str(max_length),
+                *[str(x) for x in dataset['thresholds']]]
+
+            if 'cuda' in programs[program_name]:
+                command = ['nvprof'] + command
+
             result = subprocess.run(
-                [program_name,
-                    dataset['test_file'],
-                    dataset['control_file'],
-                    str(min_length),
-                    str(max_length),
-                    *[str(x) for x in dataset['thresholds']]],
+                command,
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.PIPE,
                 universal_newlines=True)
 
-            measurements = collections.OrderedDict()
+            metrics = collections.OrderedDict()
 
-            measurements['min-length'] = min_length
-            measurements['max-length'] = max_length
+            metrics['min-length'] = min_length
+            metrics['max-length'] = max_length
+
+            # Find transfer and hash times (for CUDA)
+            if 'cuda' in programs[program_name]:
+                transfer_times = re.findall(
+                    '(\S*)\s+(?:\S+\s+){4}\[CUDA memcpy (.*)\]', result.stderr)
+
+                for time, name in transfer_times:
+                    metric_name = '{}-transfer-time'.format(name)
+                    metrics[metric_name] = time
+
+                hash_time = re.search(
+                    '(\S*)\s+(?:\S+\s+){4}void hashH3', result.stderr)
+
+                if hash_time is not None:
+                    metrics['hash-runtime'] = hash_time.group(1)
 
             # Find time reports in process output
             runtimes = re.findall(
                 '^(.*) time: ([0-9.]*)', result.stderr, re.MULTILINE)
 
             for name, time in runtimes:
-                measurements['{}-runtime [s]'.format(name.lower())] = time
+                metric_name = '{}-runtime [s]'.format(name.lower())
+                metrics[metric_name] = time
 
             # Find heavy-hitters
             heavy_hitters = re.search(
                 'Heavy-hitters \(total\): ([0-9]*)', result.stderr)
 
             if heavy_hitters is not None:
-                measurements['heavy-hitters'] = heavy_hitters.group(1)
+                metrics['heavy-hitters'] = heavy_hitters.group(1)
 
-            # Add measurements into run log
-            for x in measurements:
-                measurement_name = '{}_{}'.format(dataset_name, x)
-                if measurement_name not in runs:
-                    runs[measurement_name] = ['-'] * i + [measurements[x]]
+            # Add metrics into run log
+            for x in metrics:
+                metric_name = '{}_{}'.format(dataset_name, x)
+                if metric_name not in runs:
+                    runs[metric_name] = ['-'] * i + [metrics[x]]
                 else:
-                    runs[measurement_name].append(measurements[x])
+                    runs[metric_name].append(metrics[x])
 
             # Fill unreported measures with invalid value
             for x in runs:
